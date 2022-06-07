fe3cbee3e3dd9396aeef0a6f4ffb8b6c06dbdad6
denneth9 <wiwaxiahles@gmail.com>
2022-06-07 20:22:23 +0200

    Initial Release (Code Only)

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+namespace wtf
+{
+    class Program
+    {
+        // Main function, execution entry point of the program  
+        static void Main(string[] args) // string type parameters  
+        {
+            // Command line arguments  
+            /*//Console.WriteLine("Argument length: " + args.Length);
+            //Console.WriteLine("Supplied Arguments are:");
+            foreach (Object obj in args)
+            {
+                //Console.WriteLine(obj);
+            }*/
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter an acronym to decode");
+                return;
+            }
+            else if (args.Length > 1)
+            {
+                Console.WriteLine("Please enter only one acronym at a time");
+                return;
+            }
+            //string input = Console.ReadLine();
+            string str_current_path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string str_path = str_current_path + @"\wth.txt";
+            string message = FindInFile(args[0], str_path);
+            Console.WriteLine(message);
+
+
+            string FindInFile(string str_search, string str_path)
+            {
+                string message = "";
+                foreach (var line in File.ReadAllLines(str_path))
+                {
+                    if (!String.IsNullOrWhiteSpace(line))
+                    {
+                        int charLocation = line.IndexOf(".", StringComparison.OrdinalIgnoreCase);
+                        //if (line.IndexOf(str_search, StringComparison.OrdinalIgnoreCase) == 0)
+                        if (charLocation > 0)
+                        {
+                            string acronym = line.Substring(0, charLocation);
+                            if (acronym.ToUpper() == str_search.ToUpper())
+                            {
+                                string[] splitline = line.Split(". ");
+                                foreach (string str in splitline)
+                                {
+                                    message = message + str + System.Environment.NewLine;
+                                }
+                                return message;
+                            }
+                            //Console.WriteLine("Found one");
+
+
+                        }
+
+                    }
+
+                }
+                return "No matches found";
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # wth
+
+## Abstract
+
  A reimplementation of BSDgames' wtf acronym decoder in C#
+
+Written in about 15 minutes
+
+## Usage
+
+wth <acronym>
+
+Example: `wth wth` returns `WTH \n What the hell`
+
+## "Database" Structure
+
+The entries in wth.txt are formatted according to the following structure:
+
+```
+Mock up:
+ACRONYM. This is the definition of the acronym
+Example:
+WTH. What the hell
+WTF. What the {Fuck, Frick}. Where's the food
+```
+
+1 Entry per line, Acronyms and descriptions are separated by `. ` (Space included), multiple definitions can be added for a single acronym by simply adding another `. ` and inserting another definition.
+
+## Todo
+
+- [x] Upload to Github
+
+- [ ] Update wtf database to the wth 'standard'
+
+- [ ] Adding more acronyms
+
+- [ ] Release executable
+
+- [ ] Add to some repo?
+
+- [ ] Clean and refactor the code
+
+- [ ] Optimise the code (Alot)
